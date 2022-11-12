ed3755769a03ba22508dc2f08d503986093c731c
nnischal <nischalniroula18@gmail.com>
2022-11-12 15:37:13 +0545

    css commit

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../src/style/Style.css';
+import './style/Style.css';
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import css from './src/Style.css'
+import css from './style/Style.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
